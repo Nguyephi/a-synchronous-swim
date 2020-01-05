b8903d39f06356c8f085f7eb861fee67fe22d3f9
Phil <pronewgen92@gmail.com>
2020-01-04 20:37:11 -0500

    Make all test pass. Bare minimum.

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,45 +15,79 @@ module.exports.initialize = (queue) => {
 
 // handles the GET request
 module.exports.router = (req, res, next = ()=>{}) => {
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
+  // console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
   // conditional to check req type
   switch(req.method) {
     case 'GET':
       if (req.url === '/background.jpg') {
-        res.writeHead(200, {"content-type": "image/jpeg", ...headers});
-        fs.readFile(exports.backgroundImageFile, (err, data) => {
-          res.write(data);
+        fs.readFile(module.exports.backgroundImageFile, (err, data) => {
+          if (err) {
+            res.writeHead(404);
+          } else {
+            res.writeHead(200, {"content-type": "image/jpeg"});
+            res.write(data, 'binary');
+          }
           res.end();
+          next();
         })
       } else {
-        res.writeHead(200, {"content-type": "text/html", ...headers});
+        res.writeHead(200, {"content-type": "text/html", headers});
         var queuedMessageFromServer = messages.dequeue()
         if (queuedMessageFromServer !== undefined) {
           res.write(queuedMessageFromServer);
           res.end();
+          next();
         }
       }
       break;
     case 'POST':
-      req.on('data', directionKey => {
-        messages.enqueue(directionKey)
-      })
-      res.writeHead(200, headers);
-      res.end();
+      if (req.url === '/background.jpg') {
+        var imageData = Buffer.alloc(0);
+
+        req.on('data', (chunk) => {
+          imageData = Buffer.concat([imageData, chunk]);
+        });
+
+        req.on('end', () => {
+
+          var file = multipart.getFile(imageData);
+
+          fs.writeFile(module.exports.backgroundImageFile, file.data, (err) => {
+            res.writeHead(err ? 400 : 201);
+            res.end();
+            next();
+          })
+
+        })
+
+
+
+      } else {
+        req.on('data', directionKey => {
+          messages.enqueue(directionKey)
+        })
+        res.writeHead(201, headers);
+        res.end();
+        next()
+      }
       // filter between the file upload and the command keys being 'pressed'
       break;
     case 'OPTIONS': res.writeHead(200, headers);
       res.end()
+      next()
       break;
       // delete
       // put
-    default: res.writeHead(404, headers);
+    default:
+      res.writeHead(404, headers);
+      res.end()
+      next();
   }
 
 
   // // signifying that the response object is finished being created
   // res.end();
-  next(); // invoke next() at the end of a request to help with testing!
+  // next(); // invoke next() at the end of a request to help with testing!
 };
 
--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -26,7 +26,7 @@ describe('server responses', () => {
     // send the request to the GET of the server
     var arr = ['up', 'down', 'left', 'right'];
     let post = server.mock('/', 'POST', arr[Math.floor(Math.random() * arr.length)]);
-    httpHandler.router(post.req, post.res)
+    httpHandler.router(post.req, post.res);
     // Generate random command
 
     let {req, res} = server.mock('/', 'GET');
@@ -40,7 +40,7 @@ describe('server responses', () => {
 
   it('should respond with 404 to a GET request for a missing background image', (done) => {
     httpHandler.backgroundImageFile = path.join('.', 'spec', 'missing.jpg');
-    let {req, res} = server.mock('FILL_ME_IN', 'GET');
+    let {req, res} = server.mock('/background.jpg', 'GET');
 
     httpHandler.router(req, res, () => {
       expect(res._responseCode).to.equal(404);
@@ -49,17 +49,23 @@ describe('server responses', () => {
     });
   });
 
-  xit('should respond with 200 to a GET request for a present background image', (done) => {
-    // write your test here
-    done();
+  it('should respond with 200 to a GET request for a present background image', (done) => {
+    httpHandler.backgroundImageFile = path.join('.', 'spec', 'water-sm.jpg');
+    let {req, res} = server.mock('/background.jpg', 'GET');
+
+    httpHandler.router(req, res, () => {
+      expect(res._responseCode).to.equal(200);
+      expect(res._ended).to.equal(true);
+      done();
+    });
   });
 
-  var postTestFile = path.join('.', 'spec', 'water-lg.jpg');
+  var postTestFile = path.join('.', 'spec', 'water-lg.multipart');
 
-  xit('should respond to a POST request to save a background image', (done) => {
+  it('should respond to a POST request to save a background image', (done) => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
-      let {req, res} = server.mock('FILL_ME_IN', 'POST', fileData);
+      let {req, res} = server.mock('/background.jpg', 'POST', fileData);
 
       httpHandler.router(req, res, () => {
         expect(res._responseCode).to.equal(201);
@@ -69,15 +75,17 @@ describe('server responses', () => {
     });
   });
 
-  xit('should send back the previously saved image', (done) => {
+  it('should send back the previously saved image', (done) => {
     fs.readFile(postTestFile, (err, fileData) => {
       httpHandler.backgroundImageFile = path.join('.', 'spec', 'temp.jpg');
-      let post = server.mock('FILL_ME_IN', 'POST', fileData);
+      let post = server.mock('/background.jpg', 'POST', fileData);
 
       httpHandler.router(post.req, post.res, () => {
-        let get = server.mock('FILL_ME_IN', 'GET');
+        let get = server.mock('/background.jpg', 'GET');
         httpHandler.router(get.req, get.res, () => {
-          expect(Buffer.compare(fileData, get.res._data)).to.equal(0);
+          const multipart = require('../js/multipartUtils')
+          let file = multipart.getFile(fileData);
+          expect(Buffer.compare(file.data, get.res._data)).to.equal(0);
           done();
         });
       });
